18ee94163522022e4110cef9c6b82ee6206bda45
MuratYasar <yasar_murat@hotmail.com>
2022-03-01 13:12:10 +0300

    fixing

---
+++ MyContactList.ReportRequest.UnitTests/ApiGatewayControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MyContactList.ReportRequest.UnitTests
+{
+    public class ApiGatewayControllerTests
+    {
+        [Fact]
+        public async Task GetAllContacts_Returns_The_Correct_Number_Of_ContactsAsync()
+        {
+            var client = new TestClientProvider().Client;
+
+            // Act
+            var okResult = await client.GetAsync("values/1");
+
+            okResult.EnsureSuccessStatusCode();
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, okResult.StatusCode);
+        }
+    }
+}
